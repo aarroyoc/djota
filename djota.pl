66fdf8968a20e5e77cc665e648532d4e32f57bac
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2023-01-05 12:33:36 +0100

    Links and Images

--- djota.pl
+++ djota.pl
@@ -1,21 +1,100 @@
-:- module(djota, [inline_text//1]).
+:- module(djota, [inline_text//2]).
 
+:- use_module(library(format)).
 :- use_module(library(dcgs)).
 :- use_module(library(pio)).
 :- use_module(library(lists)).
 
-inline_text(Text) -->
-    backslash(Text).
-inline_text(Text) -->
-    ordinary_text(Text).
-inline_text([]) -->
+inline_text(Text, Data) -->
+    reference_image(Text, Data).
+inline_text(Text, Data) -->
+    inline_image(Text, Data).
+inline_text(Text, Data) -->
+    reference_link(Text, Data).
+inline_text(Text, Data) -->
+    inline_link(Text, Data).
+inline_text(Text, Data) -->
+    backslash(Text, Data).
+inline_text(Text, Data) -->
+    ordinary_text(Text, Data).
+inline_text([], _) -->
     [].
 
-backslash([Char|Text]) -->
+reference_image(Text, Data) -->
+    "![",
+    inline_text(AltText, Data),
+    "][",
+    seq(RefName),
+    "]",
+    inline_text(Text1, Data),
+    {
+	( RefName \= "" ->
+	  (
+	      member(ref(RefName, LinkUrl), Data) ->
+	      phrase(format_("<img alt=\"~s\" src=\"~s\">", [AltText, LinkUrl]), Text0)
+	  ;   phrase(format_("<img alt=\"~s\">", [AltText]), Text0) 
+	  )
+	; (
+	      member(ref(AltText, LinkUrl), Data) ->
+	      phrase(format_("<img alt=\"~s\" src=\"~s\">", [AltText, LinkUrl]), Text0)
+	  ;   phrase(format_("<img alt=\"~s\">", [AltText]), Text0)
+	  )
+	),
+	append(Text0, Text1, Text)
+    }.
+
+inline_image(Text, Data) -->
+    "![",
+    inline_text(AltText, Data),
+    "](",
+    seq(Url),
+    ")",
+    inline_text(Text1, Data),
+    {
+	phrase(format_("<img alt=\"~s\" src=\"~s\">", [AltText, Url]), Text0),
+	append(Text0, Text1, Text)
+    }.
+
+reference_link(Text, Data) -->
+    "[",
+    inline_text(LinkText, Data),
+    "][",
+    seq(RefName),
+    "]",
+    inline_text(Text1, Data),
+    {
+	( RefName \= "" ->
+	  (
+	      member(ref(RefName, LinkUrl), Data) ->
+	      phrase(format_("<a href=\"~s\">~s</a>", [LinkUrl, LinkText]), Text0)
+	  ;   phrase(format_("<a>~s</a>", [LinkText]), Text0) 
+	  )
+	; (
+	      member(ref(LinkText, LinkUrl), Data) ->
+	      phrase(format_("<a href=\"~s\">~s</a>", [LinkUrl, LinkText]), Text0)
+	  ;   phrase(format_("<a>~s</a>", [LinkText]), Text0)
+	  )
+	),
+	append(Text0, Text1, Text)
+    }.
+
+inline_link(Text, Data) -->
+    "[",
+    inline_text(LinkText, Data),
+    "](",
+    seq(LinkUrl),
+    ")",
+    inline_text(Text1, Data),
+    {
+	phrase(format_("<a href=\"~s\">~s</a>", [LinkUrl, LinkText]), Text0),
+	append(Text0, Text1, Text)
+    }.
+
+backslash([Char|Text], Data) -->
     "\\",
     [Char],
-    inline_text(Text).
+    inline_text(Text, Data).
 
-ordinary_text([Char|Text]) -->
+ordinary_text([Char|Text], Data) -->
     [Char],
-    inline_text(Text).
+    inline_text(Text, Data).
--- test.lgt
+++ test.lgt
@@ -3,6 +3,19 @@
 :- object(test, extends(lgtunit)).
 
 test(backslash) :-
-    phrase(djota:inline_text("Hello * Djota"), "Hello \\* Djota").
+    phrase(djota:inline_text("Hello * Djota", []), "Hello \\* Djota").
+
+test(link) :-
+    phrase(djota:inline_text("<a href=\"http://example.com\">My link text</a>", []), "[My link text](http://example.com)"),
+    phrase(djota:inline_text("<a href=\"http://example.com\">My link text</a>", [ref("foo bar", "http://example.com")]), "[My link text][foo bar]"),
+    phrase(djota:inline_text("<a>foo</a>", []), "[foo][bar]"),
+    phrase(djota:inline_text("<a href=\"/url\">My link text</a>", [ref("My link text", "/url")]), "[My link text][]").
+
+test(image) :-
+    phrase(djota:inline_text("<img alt=\"picture of a cat\" src=\"cat.jpg\">", []), "![picture of a cat](cat.jpg)"),
+    phrase(djota:inline_text("<img alt=\"picture of a cat\" src=\"feline.jpg\">", [ref("cat", "feline.jpg")]), "![picture of a cat][cat]"),
+    phrase(djota:inline_text("<img alt=\"cat\" src=\"feline.jpg\">", [ref("cat", "feline.jpg")]), "![cat][]").
+
+
 
 :- end_object.
