fb7fa94009682125254cf6240d6e32eb6e78e0c7
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2023-01-05 15:18:47 +0100

    AST and Heading

--- djota.pl
+++ djota.pl
@@ -1,4 +1,4 @@
-:- module(djota, [djot/2, inline_text//2]).
+:- module(djota, [djot/2, djot_ast/2, inline_text//2]).
 
 :- use_module(library(format)).
 :- use_module(library(dcgs)).
@@ -8,27 +8,76 @@
 % Block syntax
 
 djot(Djot, Html) :-
+    djot_ast(Djot, Ast),
+    once(phrase(ast_html_(Ast), Html)).
+
+djot_ast(Djot, Ast) :-
     once(phrase(lines(Lines), Djot)),
-    phrase(djot_(Lines), Html).
+    once(phrase(djot_ast_(Lines), Ast)).    
 
-%% djot_([Line|Lines], Html0, Html) :-
-%%     djot_paragraph_([Line|Lines], Html0, Html).
-%% djot_([Line|Lines], Html0, Html) :-
-%%     Line = [],
-%%     djot_(Lines, Html0, Html).
-%% djot_([], Html, Html).
+% From Djot source to AST
 
-djot_([Line|Lines]) -->
+% Thematic break
+djot_ast_([Line|Lines]) -->
+    { phrase(thematic_break_line(0), Line) },
+    [thematic_break],
+    djot_ast_(Lines).
+
+% Heading
+djot_ast_([Line|Lines]) -->
+    { phrase(heading_line(N, Header), Line) },
+    djot_heading_ast_(Lines, N, Header).
+
+% Paragraph
+djot_ast_([Line|Lines]) -->
     { Line \= "" },
-    djot_thematic_break_([Line|Lines]).
-djot_([Line|Lines]) -->
-    { Line \= "" },
-    djot_paragraph_([Line|Lines], "").
-djot_([[]|Lines]) -->
-    djot_(Lines).
-djot_([]) --> [].
+    djot_paragraph_ast_([Line|Lines], "").
 
-djot_paragraph_([Line|Lines], Paragraph0) -->
+% Empty line
+djot_ast_([[]|Lines]) -->
+    djot_ast_(Lines).
+% No more lines
+djot_ast_([]) --> [].
+
+djot_heading_ast_([Line|Lines], N, Header) -->
+    { phrase(heading_line(N, Header), Line), append(Header, [' '|Line], Header1) },
+    djot_heading_ast_(Lines, N, Header1).
+
+djot_heading_ast_([[]|Lines], N, Header) -->
+    {
+	append(SectionLines, [HeadingLine|Rest], Lines),
+	phrase(heading_line(NextN, _), HeadingLine),
+	NextN =< N,
+	phrase(djot_ast_(SectionLines), SectionAst)
+    },
+    [section(N, Header, SectionAst)],
+    djot_ast_([HeadingLine|Rest]).
+
+djot_heading_ast_([[]|Lines], N, Header) -->
+    {
+	phrase(djot_ast_(Lines), SectionAst)
+    },
+    [section(N, Header, SectionAst)].
+    
+djot_heading_ast_([], N, Header) -->
+    djot_heading_ast([""], N, Header).
+
+heading_line(1, Header) --> "# ", seq(Header).
+heading_line(2, Header) --> "## ", seq(Header).
+heading_line(3, Header) --> "### ", seq(Header).
+heading_line(4, Header) --> "#### ", seq(Header).
+heading_line(5, Header) --> "##### ", seq(Header).
+heading_line(6, Header) --> "###### ", seq(Header).
+
+section_lines([X|Xs], N) -->
+    [X], { \+ phrase(heading_line(N, _), X) },
+    section_lines(Xs, N).
+    
+section_lines([], N) -->
+    heading_line(N, _).
+section_lines([], _) --> [].
+
+djot_paragraph_ast_([Line|Lines], Paragraph0) -->
     {
 	Line \= "",
 	(
@@ -37,22 +86,15 @@ djot_paragraph_([Line|Lines], Paragraph0) -->
 	;   append(Paragraph0, [' '|Line], Paragraph1)
 	)
     },
-    djot_paragraph_(Lines, Paragraph1).
+    djot_paragraph_ast_(Lines, Paragraph1).
 
-djot_paragraph_([""|Lines], Paragraph) -->
+djot_paragraph_ast_([""|Lines], Paragraph) -->
     { phrase(inline_text(Html, []), Paragraph) },
-    "<p>",
-    Html,
-    "</p>",
-    djot_(Lines).
+    [paragraph(Html)],
+    djot_ast_(Lines).
 
-djot_paragraph_([], Paragraph) -->
-    djot_paragraph_([""], Paragraph).
-
-djot_thematic_break_([Line|Lines]) -->
-    { phrase(thematic_break_line(0), Line) },
-    "<hr>",
-    djot_(Lines).
+djot_paragraph_ast_([], Paragraph) -->
+    djot_paragraph_ast_([""], Paragraph).
 
 thematic_break_line(N) -->
     (" "|"\t"),
@@ -65,6 +107,21 @@ thematic_break_line(N0) -->
 
 thematic_break_line(N) -->
     { N >= 3 }.
+
+% From AST to HTML
+
+ast_html_([]) --> [].
+ast_html_([X|Xs]) -->
+    ast_html_node_(X),
+    ast_html_(Xs).
+
+ast_html_node_(thematic_break) -->
+    "<hr>".
+ast_html_node_(paragraph(Html)) -->
+    "<p>",Html,"</p>".
+ast_html_node_(section(N, Header, Child)) -->
+    { phrase(ast_html_(Child), ChildHtml) },
+    format_("<section><h~d>~s</h~d>~s</section>", [N, Header, N, ChildHtml]).
 
 char(X) -->
     [X],
--- test.lgt
+++ test.lgt
@@ -23,4 +23,7 @@ test(paragraph) :-
 test(thematic_break) :-
     djota:djot("Then they went to sleep.\n\n  * * * *  \n\nWhen they woke up, ...", "<p>Then they went to sleep.</p><hr><p>When they woke up, ...</p>").
 
+test(section) :-
+    djota:djot("Then they went to\n\n# sleep.\n\n  * * * *  \n\nWhen they woke up, ...\n\n# Hola\n\nIt's me", "<p>Then they went to</p><section><h1>sleep.</h1><hr><p>When they woke up, ...</p></section><section><h1>Hola</h1><p>It\'s me</p></section>").
+
 :- end_object.
